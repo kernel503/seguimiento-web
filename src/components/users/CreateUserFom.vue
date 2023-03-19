de4a82cc2d48b1ea2b6930ff6a7bde5274a6dbc2
siliezer <siliezer.marcelo@gmail.com>
2023-03-19 02:06:41 -0600

    fix: autocomplete?

--- src/components/users/CreateUserFom.vue
+++ src/components/users/CreateUserFom.vue
@@ -1,195 +1,209 @@
 <template>
   <div>
-  <!--Data table-->
-  <v-data-table
-    :headers="headers"
-    :items="items"
-    sort-by="id"
-    class="elevation-1"
-    :footer-props="{
+    <!--Data table-->
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      sort-by="id"
+      class="elevation-1"
+      :footer-props="{
         pageText: '{0}-{1} de {2}',
-        'items-per-page-text':'Elementos por página'
-    }"
+        'items-per-page-text': 'Elementos por página',
+      }"
     >
-    <template v-slot:no-data>
-      Sin registros
-    </template>
-    <template #top>
-      <v-toolbar flat>
-        <v-toolbar-title class="text-capitalize">
-          Usuarios
-        </v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-      <v-dialog v-model="dialog" width="600px" persistent>
-        <template v-slot:activator="{ on, attrs }">
-              <v-btn color="red darken-2" dark class="mb-2" v-bind="attrs" v-on="on">
+      <template v-slot:no-data> Sin registros </template>
+      <template #top>
+        <v-toolbar flat>
+          <v-toolbar-title class="text-capitalize"> Usuarios </v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" width="600px" persistent>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="red darken-2"
+                dark
+                class="mb-2"
+                v-bind="attrs"
+                v-on="on"
+              >
                 <v-icon class="mr-1"> mdi-plus-box </v-icon>
                 Nuevo registro
               </v-btn>
-        </template>
-        <v-card>
-          <v-card-title>
-           Crear registro
-          </v-card-title>
-          <v-divider class="blue accent-2 mx-5 mb-5"></v-divider>
-
-          <v-card-text>
-            <v-form ref="form" v-model="valid" >
-              <v-text-field
-                v-model="form.email"
-                outlined
-                required
-                :rules="[emailRule]"
-              >
-                <template #label>
-                  Correo electrónico
-                  <span class="red--text"><strong>* </strong></span>
-                </template>
-              </v-text-field>
-              <v-text-field
-                v-model="form.nombre_usuario"
-                label="Nombre de usuario"
-                outlined
-              ></v-text-field>
-              <v-autocomplete
-                v-model="form.rol"
-                :rules="[integerRule]"
-                item-text="name"
-                item-value="id"
-                outlined
-                :items="roles"
-              >
-                <template #label>
-                  Selecciona el rol <span class="red--text"><strong>* </strong></span>
-                </template>
-              </v-autocomplete>
-              <v-text-field
-                :rules="passwordRules"
-                v-model="form.password"
-                outlined
-                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                :type="showPassword ? 'text' : 'password'"
-                @click:append="showPassword = !showPassword"
-              >
-                <template #label>
-                  Contraseña <span class="red--text"><strong>* </strong></span>
-                </template>
-              </v-text-field>
-            </v-form>
-          </v-card-text>
-
-          <v-card-actions class="py-5">
-            <v-spacer></v-spacer>
-            <v-btn @click="close()" text> Cancelar </v-btn>
-            <v-btn @click="crearUsuario" color="red darken-1" dark>Aceptar</v-btn>
-          </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-
-    <template v-slot:item.actions="{ item }">
-      <v-icon
-        title="Cambiar estado usuario"
-        class="mr-2"
-        @click="openDialog(item)" > mdi-account-convert
-      </v-icon>
-      <v-icon
-        title="Cambiar contraseña"
-        class="mr-2"
-        @click="openDialogChangePass(item)" > mdi-lock-reset
-      </v-icon>
-    </template>
-
-    <template v-slot:item.solicitud.estado.nombre="{ item }">
-      <v-chip
-      v-if="item.solicitud.estado.id==1"
-      color="green"
-      text-color="white">{{ item.solicitud.estado.nombre }}
-    </v-chip>
-    <v-chip
-      v-else
-      color="red"
-      text-color="white">{{ item.solicitud.estado.nombre }}
-    </v-chip>
-  </template>
-</v-data-table>
-<!--Modal cambio de estado-->
-<v-dialog v-model="dialogEstado"  max-width="530px" >
-  <v-card>
-      <v-card-title
-      class="text-h5">
-      Cambio de estado
-      </v-card-title>
-      <v-card-text class="mb-0 pb-0">
-        <v-form ref="form_change_state" v-model="valid_state">
-        <v-autocomplete
-            v-model="estado"
-            :rules="[integerRule]"
-            item-text="nombre"
-            item-value="id"
-            outlined
-            :items="estados"
-          >
-            <template #label>
-              Selecciona el estado <span class="red--text"><strong>* </strong></span>
             </template>
-          </v-autocomplete>
-        </v-form>
-      </v-card-text>
+            <v-card>
+              <v-card-title> Crear registro </v-card-title>
+              <v-divider class="blue accent-2 mx-5 mb-5"></v-divider>
 
-      <v-card-actions class="mt-0 pt-0">
-      <v-spacer></v-spacer>
-      <v-btn class="default" color=""  @click="dialogEstado = false" >Cancelar</v-btn>
-      <v-btn color="red darken-3" @click="changeState()" dark> Aceptar </v-btn>
-      </v-card-actions>
-  </v-card>
-</v-dialog>
-<!--Modal cambio de contraseña-->
-<v-dialog v-model="dialogCambioPassword"  max-width="530px" >
-  <v-card>
-      <v-card-title
-      class="text-h5">
-      Cambiar contraseña
-      </v-card-title>
-      <v-card-text>
-        <v-form ref="form_change_pass">
-        <v-text-field
-          :rules="[fieldRule]"
-          v-model="editedItem.password"
-          outlined
-          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="showPassword ? 'text' : 'password'"
-          @click:append="showPassword = !showPassword"
-        >
-          <template #label>
-            Contraseña <span class="red--text"><strong>* </strong></span>
-          </template>
-        </v-text-field>
+              <v-card-text>
+                <v-form ref="form" v-model="valid">
+                  <v-text-field
+                    v-model="form.email"
+                    outlined
+                    required
+                    :rules="[emailRule]"
+                  >
+                    <template #label>
+                      Correo electrónico
+                      <span class="red--text"><strong>* </strong></span>
+                    </template>
+                  </v-text-field>
+                  <v-text-field
+                    v-model="form.nombre_usuario"
+                    label="Nombre de usuario"
+                    outlined
+                    autocomplete="off"
+                  ></v-text-field>
+                  <v-autocomplete
+                    v-model="form.rol"
+                    :rules="[integerRule]"
+                    item-text="name"
+                    item-value="id"
+                    outlined
+                    :items="roles"
+                  >
+                    <template #label>
+                      Selecciona el rol
+                      <span class="red--text"><strong>* </strong></span>
+                    </template>
+                  </v-autocomplete>
+                  <v-text-field
+                    :rules="passwordRules"
+                    v-model="form.password"
+                    outlined
+                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="showPassword ? 'text' : 'password'"
+                    @click:append="showPassword = !showPassword"
+                    autocomplete="off"
+                  >
+                    <template #label>
+                      Contraseña
+                      <span class="red--text"><strong>* </strong></span>
+                    </template>
+                  </v-text-field>
+                </v-form>
+              </v-card-text>
 
-        <v-text-field
-          :rules="[fieldRule,repeatPasswordRules]"
-          v-model="passConfirm"
-          outlined
-          :append-icon="showPasswordConfirm ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="showPasswordConfirm ? 'text' : 'password'"
-          @click:append="showPasswordConfirm = !showPasswordConfirm"
+              <v-card-actions class="py-5">
+                <v-spacer></v-spacer>
+                <v-btn @click="close()" text> Cancelar </v-btn>
+                <v-btn @click="crearUsuario" color="red darken-1" dark
+                  >Aceptar</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+
+      <template v-slot:item.actions="{ item }">
+        <v-icon
+          title="Cambiar estado usuario"
+          class="mr-2"
+          @click="openDialog(item)"
         >
-          <template #label>
-            Confirmar Contraseña <span class="red--text"><strong>* </strong></span>
-          </template>
-        </v-text-field>
-      </v-form>
-      </v-card-text>
-      <v-card-actions class="py-3">
-      <v-spacer></v-spacer>
-      <v-btn class="default" text  @click="dialogCambioPassword = false" >Cancelar</v-btn>
-      <v-btn class="red darken-2" dark @click="changePassword()">Aceptar</v-btn>
-      </v-card-actions>
-  </v-card>
-</v-dialog>
- </div>
+          mdi-account-convert
+        </v-icon>
+        <v-icon
+          title="Cambiar contraseña"
+          @click="openDialogChangePass(item)"
+        >
+          mdi-lock-reset
+        </v-icon>
+      </template>
+
+      <template v-slot:item.solicitud.estado.nombre="{ item }">
+        <v-chip
+          v-if="item.solicitud.estado.id == 1"
+          small
+          color="green"
+          text-color="white"
+        >
+          {{ item.solicitud.estado.nombre }}
+        </v-chip>
+        <v-chip v-else color="red" text-color="white" small>
+          {{ item.solicitud.estado.nombre }}
+        </v-chip>
+      </template>
+    </v-data-table>
+    <!--Modal cambio de estado-->
+    <v-dialog v-model="dialogEstado" max-width="530px">
+      <v-card>
+        <v-card-title class="text-h5"> Cambio de estado </v-card-title>
+        <v-card-text class="mb-0 pb-0">
+          <v-form ref="form_change_state" v-model="valid_state">
+            <v-autocomplete
+              v-model="estado"
+              :rules="[integerRule]"
+              item-text="nombre"
+              item-value="id"
+              outlined
+              :items="estados"
+            >
+              <template #label>
+                Selecciona el estado
+                <span class="red--text"><strong>* </strong></span>
+              </template>
+            </v-autocomplete>
+          </v-form>
+        </v-card-text>
+
+        <v-card-actions class="mt-0 pt-0">
+          <v-spacer></v-spacer>
+          <v-btn class="default" color="" @click="dialogEstado = false"
+            >Cancelar</v-btn
+          >
+          <v-btn color="red darken-3" @click="changeState()" dark>
+            Aceptar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!--Modal cambio de contraseña-->
+    <v-dialog v-model="dialogCambioPassword" max-width="530px">
+      <v-card>
+        <v-card-title class="text-h5"> Cambiar contraseña </v-card-title>
+        <v-card-text>
+          <v-form ref="form_change_pass">
+            <v-text-field
+              :rules="[fieldRule]"
+              v-model="editedItem.password"
+              outlined
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="showPassword ? 'text' : 'password'"
+              @click:append="showPassword = !showPassword"
+            >
+              <template #label>
+                Contraseña <span class="red--text"><strong>* </strong></span>
+              </template>
+            </v-text-field>
+
+            <v-text-field
+              :rules="[fieldRule, repeatPasswordRules]"
+              v-model="passConfirm"
+              outlined
+              :append-icon="showPasswordConfirm ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="showPasswordConfirm ? 'text' : 'password'"
+              @click:append="showPasswordConfirm = !showPasswordConfirm"
+            >
+              <template #label>
+                Confirmar Contraseña
+                <span class="red--text"><strong>* </strong></span>
+              </template>
+            </v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions class="py-3">
+          <v-spacer></v-spacer>
+          <v-btn class="default" text @click="dialogCambioPassword = false"
+            >Cancelar</v-btn
+          >
+          <v-btn class="red darken-2" dark @click="changePassword()"
+            >Aceptar</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 <script>
 import {
@@ -218,16 +232,17 @@ export default {
       password: '',
     },
     headers: [
-      {
-        text: 'Usuario',
-        align: 'start',
-        value: 'name',
-      },
-      { text: 'Correo', align: 'center', value: 'email' },
+      // {
+      //   text: 'Usuario',
+      //   align: 'start',
+      //   value: 'name',
+      // },
+      { text: 'Correo', align: 'start', value: 'email' },
+      { text: 'Rol', align: 'start', value: 'roles' },
       { text: 'Estado', align: 'center', value: 'solicitud.estado.nombre' },
       {
         text: 'Acciones',
-        align: 'right',
+        align: 'start',
         value: 'actions',
         sortable: false,
       },
@@ -243,7 +258,9 @@ export default {
     estado: '',
     estados: [],
     passwordRules: [
-      (v) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/.test(v)
+      (v) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/.test(
+        v,
+      )
         || 'La contraseña debe tener al entre 8, al menos un dígito, al menos tres minúscula y al menos una mayúscula.',
     ],
   }),
@@ -252,7 +269,9 @@ export default {
     emailRule: email('Debe agregar un correo válido.'),
     fieldRule: string('Debe completar el campo.'),
     integerRule: integer('Debe seleccionar un rol.'),
-    passwordRule: password('La contraseña debe tener mínimo una letra mayúscula, tres minúscula, un carácter especial, un dígito y mínimo ocho caracteres'),
+    passwordRule: password(
+      'La contraseña debe tener mínimo una letra mayúscula, tres minúscula, un carácter especial, un dígito y mínimo ocho caracteres',
+    ),
     async obtenerRoles() {
       const response = await this.axios.get('/roles');
       const {
@@ -266,8 +285,7 @@ export default {
       if (!this.$refs.form.validate()) return;
 
       try {
-        const path = this.$route.name === 'signup'
-          ? '/usuario' : '/usuario/admin';
+        const path = this.$route.name === 'signup' ? '/usuario' : '/usuario/admin';
 
         const { data } = await this.axios.post(path, { ...this.form });
 
@@ -286,8 +304,18 @@ export default {
       }
     },
     async initialize() {
-      const response = await this.axios.get('/usuarios?include=solicitud.estado');
-      this.items = response.data.data;
+      const params = new URLSearchParams();
+      // params.append('include', 'solicitud.estado');
+      params.append('include', 'roles');
+
+      const response = await this.axios.post('/usuarios/search', {
+        includes: [{ relation: 'roles' }, { relation: 'solicitud.estado' }],
+      });
+
+      this.items = response.data.data.map((user) => ({
+        ...user,
+        roles: user.roles.map((rol) => rol.name).join(', '),
+      }));
     },
     openDialog(item) {
       this.editedIndex = this.items.indexOf(item);
@@ -309,7 +337,10 @@ export default {
         const validate = this.$refs.form_change_state.validate();
         if (validate) {
           const data = { id_estado_solicitud: nuevoEstado };
-          await this.axios.put(`/solicitudes-cuentas/${this.editedItem.id}`, data);
+          await this.axios.put(
+            `/solicitudes-cuentas/${this.editedItem.id}`,
+            data,
+          );
           this.initialize();
           this.$toast.success('Registro actualizado');
           this.dialogEstado = false;
@@ -347,7 +378,8 @@ export default {
   },
   computed: {
     repeatPasswordRules() {
-      return () => this.editedItem.password === this.passConfirm || 'Contraseñas no coinciden';
+      return () => this.editedItem.password === this.passConfirm
+        || 'Contraseñas no coinciden';
     },
   },
 };
