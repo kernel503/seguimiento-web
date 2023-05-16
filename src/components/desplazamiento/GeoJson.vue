2d481947e3e6ffb5452192472840d214067396af
kernel503 <juanko682@gmail.com>
2023-05-16 15:55:03 +0000

    fix: color

--- src/components/desplazamiento/GeoJson.vue
+++ src/components/desplazamiento/GeoJson.vue
@@ -1,34 +1,16 @@
 <template>
   <v-card>
     <v-card-title>
-      <v-btn
-        text
-        class="mb-1 pb-1 pt-0 mt-0"
-        color="red darken-2"
-        @click="$router.go(-1)"
-      >
+      <v-btn text class="mb-1 pb-1 pt-0 mt-0" color="red darken-2" @click="$router.go(-1)">
         Regresar
       </v-btn>
     </v-card-title>
     <v-card-text>
       <v-img height="72vh" width="100vw">
-        <l-map
-          v-if="!false"
-          :style="{ position: 'absolute', height:'100%', width:'100%' }"
-          :zoom="config.zoom"
-          :center="config.center"
-          ref="map"
-          @ready="ready"
-        >
-          <l-tile-layer
-            :url="config.url"
-            :attribution="config.attribution"
-          ></l-tile-layer>
-          <l-geo-json
-            v-if="geojson"
-            :geojson="geojson"
-            :options="geojsonOptions"
-          >
+        <l-map v-if="!false" :style="{ position: 'absolute', height: '100%', width: '100%' }" :zoom="config.zoom"
+          :center="config.center" ref="map" @ready="ready">
+          <l-tile-layer :url="config.url" :attribution="config.attribution"></l-tile-layer>
+          <l-geo-json v-if="geojson" :geojson="geojson" :options="geojsonOptions">
           </l-geo-json>
         </l-map>
       </v-img>
@@ -36,10 +18,10 @@
   </v-card>
 </template>
 <script>
-import { LMap, LTileLayer, LGeoJson } from 'vue2-leaflet';
+import { LMap, LTileLayer, LGeoJson } from "vue2-leaflet";
 
 export default {
-  name: 'GeoJson',
+  name: "GeoJson",
 
   components: {
     LMap,
@@ -51,43 +33,44 @@ export default {
     // 3bfc0a52-2e00-4225-86e7-92fd2cbfc376
     // 1,5,2,1
     return {
+      selectedId: null,
       geojson: null,
       geojsonOptions: {
         style(feature) {
           return {
-            color: feature.properties.color || 'red',
-            weight: 7,
+            color: feature.properties.color || "rgba(255,26,104,1)",
+            weight: 6,
           };
         },
         onEachFeature(feature, layer) {
           console.log(layer);
           console.log(feature);
-          layer.bindPopup('Hi There!');
-          setTimeout(() => {
-            // eslint-disable-next-line no-param-reassign
-            layer.options.weight = 15;
-          }, 1000);
+          // layer.bindPopup('Hi There!');
+          // setTimeout(() => {
+          //   // eslint-disable-next-line no-param-reassign
+          //   layer.options.weight = 30;
+          // }, 1000);
         },
       },
       leafletProviders: [
         {
-          name: 'OpenStreetMap',
+          name: "OpenStreetMap",
           visible: true,
           attribution:
             '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-          url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         },
         {
-          name: 'Stadia.AlidadeSmoothDark',
+          name: "Stadia.AlidadeSmoothDark",
           visible: false,
-          url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
+          url: "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
           attribution:
             '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
         },
       ],
 
       config: {
-        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         attribution:
           '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
         zoom: 10,
@@ -108,13 +91,11 @@ export default {
       const { uuid } = this.$route.params;
 
       const {
-        data: { coleccion, limite },
-      } = await this.axios.get(`desplazamiento/${uuid}`, {
-        params: { tipo: 'geojson' },
-      });
+        data: { coleccion },
+      } = await this.axios.get(`desplazamiento/${uuid}`);
 
       this.geojson = coleccion;
-      this.mapa.flyToBounds(limite);
+      // this.mapa.flyToBounds(limite);
     },
   },
 };
